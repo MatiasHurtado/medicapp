13c406bafeb6c27f17fcefca81cf780d0a8cdeeb
Matias <matias.wolcraft3@gmail.com>
2021-08-09 14:45:47 -0400

    Arreglando errores

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import React,{useState} from 'react';
 
 function App() {
   const [horarios,guardarHorario] = useState([
+    {Fecha:"20/02/2019",id:1 ,idMedico:3},
+    {Fecha:"10/04/2019",id:1 ,idMedico:4},
+    {Fecha:"30/03/2019",id:1 ,idMedico:5},
+    {Fecha:"10/01/2019",id:1 ,idMedico:2},
     {Fecha:"20/02/2019",id:1 ,idMedico:1},
-    {Fecha:"10/04/2019",id:1 ,idMedico:1},
-    {Fecha:"30/03/2019",id:1 ,idMedico:1},
-    {Fecha:"10/01/2019",id:1 ,idMedico:1}
+    {Fecha:"10/04/2019",id:1 ,idMedico:5},
+    {Fecha:"30/03/2019",id:1 ,idMedico:6},
+    {Fecha:"10/01/2019",id:1 ,idMedico:2},
+    {Fecha:"20/02/2019",id:1 ,idMedico:6},
+    
   ]);
   const [horariomedi,guardarHorarioMed] = useState([])
   const [medicos,guardarMedico] =useState([
@@ -16,19 +22,27 @@ function App() {
     {id:2,nombre:"Fernando Guitierrez", especialidad:"Ginecologo"},
     {id:3,nombre:"Luis Estrella", especialidad:"Dentista"},
     {id:4,nombre:"Thiare Zenteno", especialidad:"Cosmetologia"},
+    {id:3,nombre:"Luis Estrella", especialidad:"Dentista"},
+    {id:4,nombre:"Thiare Zenteno", especialidad:"Cosmetologia"},
+    {id:3,nombre:"Luis Estrella", especialidad:"Dentista"},
+    {id:4,nombre:"Thiare Zenteno", especialidad:"Cosmetologia"},
   
   ])
 
- const  {nombre,especialidad}= medicos
+ const  {nombre,especialidad,id}= medicos
 
-
-
+ const verhorario =(id) =>{
+  const result = horarios.filter(horario => horario.idMedico === id);
+  guardarHorarioMed(result)
+  }
   return (
 
 
     <div className="Contenerdor">
       <div className="cabecera">
-        <h1>Hola Cliente</h1>
+        <div className="titulo">
+          <h1>Hola Cliente</h1>
+        </div>
       </div>
       <div className="productos">
         {/* Medicos Disponibles */}
@@ -40,7 +54,9 @@ function App() {
                 <li className="listado">
                 <div class="box">
                   <p>Nombre: {medico.nombre} especialidad: {medico.especialidad}</p>
-                  <button class="button is-primary is-small is-rounded">Pedir Hora</button>
+                  <button class="button is-primary is-small is-rounded"
+                    onClick={()=>verhorario(medico.id)}
+                  >Pedir Hora</button>
                 </div>
               </li>
               ))}
@@ -52,11 +68,10 @@ function App() {
         <div className="producto hor">
           <h3>Horario Disponible</h3>
           <ul className="listados">
-          
-              {horarios.map(hora => (
+              {horariomedi.map(hora => (
                 <li className="listado">
                 <div class="box">
-                  <p>Fecha: {hora.Fecha}</p>
+                  <p>Fecha: {hora.Fecha} </p>
                   <button class="button is-primary is-small is-rounded">Pedir Hora</button>
                 </div>
               </li>
